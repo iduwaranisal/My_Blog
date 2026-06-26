bd7c44e348cfc16273921a803f20402d5ad57b35
Iduwara nisal <induwaranisal7158980@gmail.com>
2026-06-26 13:19:03 +0530

    Fix CSRF trusted origins for Railway

--- AI_Portfolio/portfolio_core/settings.py
+++ AI_Portfolio/portfolio_core/settings.py
@@ -123,3 +123,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
